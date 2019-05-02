d2bf8faae310d4872736589a86a75c24c4174606
aneeshev <aneeshev007@gmail.com>
2019-05-02 11:14:35 +0000

    added zlib

--- sample_code/readable-stream.js
+++ sample_code/readable-stream.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var zlib = require('zlib');
 var fs = require('fs');
 
 var s = request('http://pluralsight.com/');
@@ -18,4 +19,8 @@ s.on('end', function(){
 //using pipe() concept.
 request('http://pluralsight.com/').pipe(process.stdout);
 
+// To write the content.
 request('http://pluralsight.com/').pipe(fs.createWriteStream('pluralsight.html'));
+
+// To zip the file.
+request('http://pluralsight.com/').pipe(zlib.createGzip()).pipe(fs.createWriteStream('pluralsight.zip'));
