3002ec9f211d099faf3d122ff7b0956de9da80c5
aneeshev <aneeshev007@gmail.com>
2019-05-02 07:55:12 +0000

    request module

--- sample_code/readable-stream.js
+++ sample_code/readable-stream.js
@@ -2,6 +2,10 @@ var request = require('request');
 
 var s = request('http://pluralsight.com/');
 
+// s.on('start', function(){
+//     console.log('Begin');
+// });
+
 s.on('data', function(chunk){
     console.log(">>>Data>>>"+chunk);
 });
